e53a2caf71ec10b45369ce96f5f8eae3283cc68d
尹学江 <l>
2016-10-13 11:02:45 +0800

    注册，登陆，首页导航效果

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var storage = multer.diskStorage({
     cb(null, 'www/uploads');
   },
   filename: function(req, file, cb) {
-  	console.log(file);
+    console.log(file);
     cb(null, file.originalname);
   }
 });
@@ -22,7 +22,64 @@ app.use(cookieParser());
 
 app.get('', (req, res) => {
   console.log('这是根目录');
-  res.json({"code":"",{}})
+})
+
+// 用户注册
+app.post('/user/register', (req, res) => {
+  // 增加ip
+  req.body.ip = req.ip;
+  // 增加时间
+  req.body.time = new Date().getTime();
+  // 注册用户对象
+  var user = req.body;
+  if (user.password == user.password01) {
+    // 删除一个
+    delete user.password01;
+    console.log(user);
+    // 读取用户文件
+    fs.readFile("users/user.txt", (err, data) => {
+      var users = data.toString().trim();
+      var douhao = users.length > 0 ? "," : "";
+      var usersArr = JSON.parse("[" + users + "]");
+      var isIn = usersArr.some(function(ele) {
+        return (user.name == ele.name);
+      })
+      if (isIn) {
+        res.status(200).json({ "code": "error", "content": "用户名已存在！" });
+      } else {
+        fs.appendFile("users/user.txt", douhao + JSON.stringify(user), (err) => {
+          if (!err) {
+            res.status(200).json({ "code": "success", "content": "恭喜，注册成功！" });
+          }
+        })
+      }
+
+    })
+  } else {
+    res.status(200).json({ "code": "error", "content": "密码输入不致！" });
+  }
+
+
+
+
+  // console.log('这是根目录');
+})
+// 用户登陆
+app.post('/user/login', (req, res) => {
+  console.log(req.body);
+  var user = req.body;
+  fs.readFile("users/user.txt",(err,data)=>{
+    var users = data.toString().trim();    
+    var usersArr = JSON.parse("["+users +"]");
+    var isIn = usersArr.some(function(ele){
+      return (user.name == ele.name && user.password == ele.password)
+    });
+    if(isIn){
+      res.status(200).json({code:"success",content:"登陆成功！",data:user})
+    }else{
+      res.status(200).json({code:"error",content:"用户名或密码不正确！"})
+    }
+  })
 })
 
 app.listen(3000, () => {
--- www/js/index.js
+++ www/js/index.js
@@ -1,12 +1,5 @@
 $(function() {
-  // 控制用户下拉的位置
-  $("body").on("click", ".user-name", function() {
-    $(".user-info").css({
-      top: 50 + "px",
-      right: -7 + "px"
-    });
-    $(".user-info").slideToggle();
-  })
+  
 
 });
 // 尺寸发改变时
